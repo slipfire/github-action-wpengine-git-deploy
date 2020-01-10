f2c30bb629bbee898dd8c5dc4e5b265c47129853
Slam <slamorte@gmail.com>
2020-01-09 19:08:07 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,17 +28,16 @@ chmod 644 "$KNOWN_HOSTS_PATH"
 chmod 600 "$WPENGINE_SSH_KEY_PRIVATE_PATH"
 chmod 644 "$WPENGINE_SSH_KEY_PUBLIC_PATH"
 
-printf "\nlocal_branch = $BRANCH\n"
 git config core.sshCommand "ssh -i $WPENGINE_SSH_KEY_PRIVATE_PATH -o UserKnownHostsFile=$KNOWN_HOSTS_PATH"
 git remote add $WPENGINE_SITE_NAME git@$WPENGINE_HOST:$WPENGINE_ENV/$WPENGINE_SITE_NAME.git
 printf "\nlocal_branch = $BRANCH\n"
-printf "\nshow remote $WPENGINE_ENV:\n"
-git remote show $WPENGINE_ENV
-printf "\nlocal branches:\n"
+printf "\nshow remote $WPENGINE_SITE_NAME:\n"
+git remote show $WPENGINE_SITE_NAME
+printf "\n\nlocal branches:\n"
 git branch
-printf "\nremote branche:\n"
+printf "\n\nremote branche:\n"
 git branch -r
-printf "\ngit status:\n"
+printf "\n\ngit status:\n"
 git status
-printf "\nPushing to site = $WPENGINE_SITE_NAME\n"
+printf "\n\nPushing to site = $WPENGINE_SITE_NAME\n"
 git push -fu $WPENGINE_SITE_NAME $BRANCH:master
